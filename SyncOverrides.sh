2b9a797ea76213ca117b31e68f84612d521f1d58
Michael Schuler <mschuler@a-f.ch>
2018-06-20 17:50:18 +0200

    getestetes Script v1

--- SyncOverrides.sh
+++ SyncOverrides.sh
@@ -17,9 +17,9 @@ LOCAL_OVERRIDE_DIR=$(defaults read com.github.autopkg RECIPE_OVERRIDE_DIRS)
 # Klont WEB_OVERRIDE_DIR per git, falls im Override-Ordner noch kein .git vorhanden ist
 if [ -e "$LOCAL_OVERRIDE_DIR/.git" ]
 then
-echo "Initialer 'git clone' wurde schon erfolgreich ausgeführt!"
+  echo "Initialer 'git clone' wurde schon erfolgreich ausgeführt!"
 else
-git clone "$WEB_OVERRIDE_DIR" "$LOCAL_OVERRIDE_DIR"
+  git clone "$WEB_OVERRIDE_DIR" "$LOCAL_OVERRIDE_DIR"
 fi
 
 # setzt master als aktiver branch.
@@ -35,9 +35,25 @@ git -C "$LOCAL_OVERRIDE_DIR" pull
 OVERRIDEFILES="$LOCAL_OVERRIDE_DIR/*"
 for f in $OVERRIDEFILES
 do
-  yes | autopkg update-trust-info $f
+  if [[ "$f" != *"README.md"* ]]
+  then
+    yes | autopkg update-trust-info $f
+  fi
 done
 
+# aktiviert alle Overrides in AutoPkgr recipe_list.txt in ""/Users/AKTIVER_USER/Library/Application Support/AutoPkgr"
+RECIPE_LIST_TXT="$HOME/Library/Application Support/AutoPkgr/recipe_list.txt"
+rm "$RECIPE_LIST_TXT"
+OVERRIDEFILES="$LOCAL_OVERRIDE_DIR/*"
+for f in $OVERRIDEFILES
+do
+  if [[ "$f" != *"README.md"* ]]
+  then
+    RECIPE_LIST_TXT_ELEMENT=$(/usr/libexec/PlistBuddy -c "Print :Identifier" $f)
+    echo "$RECIPE_LIST_TXT_ELEMENT" >> "$RECIPE_LIST_TXT"
+  fi
+done
+echo "MakeCatalogs.munki" >> "$RECIPE_LIST_TXT"
 
 
 exit 0
